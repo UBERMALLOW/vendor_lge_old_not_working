20fe5505184ff473248330578b23708e8caf7332
Chet Kener <Cl3Kener@gmail.com>
2015-10-12 09:38:04 -0400

    Make sure to include symbolic links
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- hammerhead/proprietary/Android.mk
+++ hammerhead/proprietary/Android.mk
@@ -86,4 +86,24 @@ LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libacdbloader
+LOCAL_SRC_FILES := libacdbloader.so
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_OWNER := qcom
+
+# Create symbolic link because user space can access persist directory,
+# while kernel ALSA drivers can only access the /system/etc/firmware directory
+LOCAL_POST_INSTALL_CMD := \
+    mkdir -p $(TARGET_OUT_ETC)/firmware/wcd9310; \
+        ln -sf /data/misc/audio/wcd9310_anc.bin \
+        $(TARGET_OUT_ETC)/firmware/wcd9310/wcd9310_anc.bin; \
+        ln -sf /data/misc/audio/mbhc.bin \
+        $(TARGET_OUT_ETC)/firmware/wcd9310/wcd9310_mbhc.bin
+
+#include $(BUILD_PREBUILT)
+
 endif
